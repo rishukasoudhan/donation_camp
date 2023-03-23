541f281ac29fb87ed2ea4ef8b1cea3f171b54ad6
Shivam <shivamkasoudhan7@gmail.com>
2023-03-23 11:28:11 +0530

    adding donation.sol and setup

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+module.exports = {
+  solidity:{
+    version :"0.8.0"
+  },
+  path :{
+    artifacts :"./client/src/artifacts"
+  }
+};
